7c04e32c10a381455b2b547191b638443ebde843
Margse <142811987+Margse@users.noreply.github.com>
2024-02-07 08:37:43 +0100

    Update FractionTester.java

--- FractionTester.java
+++ FractionTester.java
@@ -1,21 +1,33 @@
 public class FractionTester {
+    final double PI = 3.14159;
+
     public static void main(String[] args) {
         Fraction f = new Fraction();
-        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
-        f.numerator = 12;
-        f.denominator = 0;
-        System.out.println(f.numerator + "/" + f.denominator);
-        
+        Fraction h = new Fraction(2, 1);
+        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat
+        // sind:
+        f.setValues(12, 1);
+        System.out.println(f.getNumerator() + "/" + f.getDenominator());
+
         // toString():
-
+        System.out.println(f.toString());
         // Encapsulation, Getter und Setter:
-
+        f.setValues(24, 12);
+        System.out.println(f.toString());
         // Copy-Konstruktor und equals():
-        
+        Fraction g = new Fraction(f);
+        System.out.println(g.equals(f));
+        System.out.println(g == f ? true : false);
+        System.out.println(g.equals(h));
         // Klassen- und Instanzenmethoden:
-
+        f.add(g);
+        System.out.println(f.toString());
+        System.out.println(Fraction.add(f, g));
         // statische Variablen:
+        System.out.println(Fraction.getNumberOfFractions());
 
+        g.setValues(25, 8);
+        System.out.println(g.toString());
     }
 
 }
